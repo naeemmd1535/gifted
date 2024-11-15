d64ab81fb75818957c1a4d09f631540fca2c45da
naeemmd1535 <naeemmd1535@gmail.com>
2024-11-15 20:18:40 +0500

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -2,7 +2,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Gifted~4059cEqY",
+  SESSION_ID: process.env.SESSION_ID || "Gifted~cQ9cR1Vy",
   PREFIX: process.env.PREFIX || '.',
   AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || '💚', // For liking whatsapp status updates(stories)
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS !== undefined ? process.env.AUTO_READ_STATUS === 'true' : true,
@@ -23,8 +23,8 @@ const config = {
   MODE: process.env.MODE || "private",
   OWNER_NAME: process.env.OWNER_NAME || "ɢɪғᴛᴇᴅ ᴛᴇᴄʜ",
   AUTO_BLOCK: process.env.AUTO_BLOCK || "212,99,90",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "923426772448",
-  SUDO_NUMBERS: process.env.SUDO_NUMBERS || "923426772448",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "923341545354",
+  SUDO_NUMBERS: process.env.SUDO_NUMBERS || "923341545354",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyAkHGe6qWMujR2BjM468vQbY2RIbp0VXyc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
